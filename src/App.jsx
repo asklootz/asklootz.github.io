517cc32771c3cdcef7336dcdc5321e506730c3dc
asklootz <askl@uia.no>
2025-03-03 15:02:06 +0100

    Created site
    Created site in React, working on navbar
    finished loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import  { useState } from 'react';
 import './App.css';
+import "./index.css";
 import { LoadingScreen } from './components/LoadingScreen';
+import "./index.css";
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
   return (
-  <>
-    <LoadingScreen />
-  </>
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      <div 
+      className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"}
+      bg-black`}
+      >
+
+      </div>
+    </>
   );
 }
 
